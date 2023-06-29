6e86a2b7db77e1d36866e9da4861453c1bf86d8d
Andrea <andita54@hotmail.com>
2023-06-29 16:35:21 +1000

    adding search button

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,31 +2,35 @@ import {React, useState} from 'react'
 import Card from './Card'
 import axios from 'axios'
 
+
 function Main() {
     const[search ,setSearch] = useState("");
     const [bookData, setData]=useState([]);
+
+    const searchTextHandler = (event) => {
+      setSearch(event.target.value);
+    };
+    
     const searchBook = (evt)=>{
-            if(evt.key==="Enter")
-            {
+
                 axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyC_haRGVtUMkH3WoTGFxIzFrQhJhtpYB2o +&maxResults=40')
                 .then(res=>setData(res.data.items))
                 .catch(err=>console.log(err))
-            }
+            
     }
   return (
     <>
         <div className="header">
             <div className="row1">
-            
-            </div>
-            <div className="row2">
                 <h2>Find your book</h2>
                 <div className="search">
-                    <input type="text" placeholder=" Type your book's name then Enter"
-                    value={search} onChange={e=>{setSearch(e.target.value)}}
-                    onKeyPress={searchBook}
+                    <input type="text" placeholder=" Book's Topic"
+                    value={search} 
+                    onChange={searchTextHandler}
                     />
-                 
+                  <div className="button">
+                    <button onClick={searchBook}><strong>Search</strong></button>
+                  </div>
                     </div>
                     <img src="./images/books2.jpg" alt="book" />
             </div>
